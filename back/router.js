5cdd00dea1d7aacd6e237d1f3f8583f314cfb7ba
IsmoilKarimov <karimoovismooil@gmail.com>
2022-08-24 18:52:18 +0500

    Registration page is completed

--- back/router.js
+++ back/router.js
@@ -2,8 +2,14 @@ const {Router} = require('express')
 const router = Router()
 const auth = require('./middleware/auth')
 
+const userRouter = require('./router/user')
+
 router.get('/',auth,async(req,res)=>{
-    res.render('index')
+    res.render('index',{
+        title: 'Boshqaruv sahifasi',
+    })
 })
+
+router.use('/user',userRouter)
 
 module.exports = router
