4500d6c88dae6ff5070fbfa59c63c9d4a041a224
IsmoilKarimov <karimoovismooil@gmail.com>
2022-10-24 19:50:17 +0500

    .

--- front/src/components/home/fast.js
+++ front/src/components/home/fast.js
@@ -1,17 +1,35 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 //images
 import book from '../../assets/img/book.jpg'
 
 const Fast = () => {
+    
+    const [promo, setPromo] = useState({
+        products: []
+    })
+
+    useEffect(()=>{
+        axios.get('http://localhost:3003/promo/last')
+        .then(res => {
+            let p = res.data
+            p.products = p.products.map(product => {
+                product.img = product.img[0]
+                return product
+            })
+            setPromo(res.data)
+        })
+    })
+
     return(
         <div className="fast">
             <div className="container">
-                <div className="btitle text-center mb-1">Tezkor aksiya</div>
-                <div className="fast__stitle text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </div>
+                <div className="btitle text-center mb-1">{promo.title}</div>
+                <div className="fast__stitle text-center">{promo.description}</div>
                 <div className="fast__timer">
                     <div className="fast__items">
                         <div className="fast__val">02</div>
@@ -30,52 +48,24 @@ const Fast = () => {
                         <div className="fast__name">Soniya</div>
                     </div>
                 </div>
-                <div className="d-flex justify-content-between">
-                    <Link to="/" className="fast__box">
-                        <div className="fast__img" style={{
-                            backgroundImage:`url(${book})`
-                        }}></div>
-                        <div className="fast__title">Ibtido</div>
-                        <div className="fast__genre">Datektiv</div>
-                        <div className="fast__old">120 000 so'm</div>
-                        <div className="fast__new">80 000 so'm</div>
-                    </Link>
-                    <Link to="/" className="fast__box">
-                        <div className="fast__img" style={{
-                            backgroundImage:`url(${book})`
-                        }}></div>
-                        <div className="fast__title">Ibtido</div>
-                        <div className="fast__genre">Datektiv</div>
-                        <div className="fast__old">120 000 so'm</div>
-                        <div className="fast__new">80 000 so'm</div>
-                    </Link>
-                    <Link to="/" className="fast__box">
-                        <div className="fast__img" style={{
-                            backgroundImage:`url(${book})`
-                        }}></div>
-                        <div className="fast__title">Ibtido</div>
-                        <div className="fast__genre">Datektiv</div>
-                        <div className="fast__old">120 000 so'm</div>
-                        <div className="fast__new">80 000 so'm</div>
-                    </Link>
-                    <Link to="/" className="fast__box">
-                        <div className="fast__img" style={{
-                            backgroundImage:`url(${book})`
-                        }}></div>
-                        <div className="fast__title">Ibtido</div>
-                        <div className="fast__genre">Datektiv</div>
-                        <div className="fast__old">120 000 so'm</div>
-                        <div className="fast__new">80 000 so'm</div>
-                    </Link>
-                    <Link to="/" className="fast__box">
-                        <div className="fast__img" style={{
-                            backgroundImage:`url(${book})`
-                        }}></div>
-                        <div className="fast__title">Ibtido</div>
-                        <div className="fast__genre">Datektiv</div>
-                        <div className="fast__old">120 000 so'm</div>
-                        <div className="fast__new">80 000 so'm</div>
-                    </Link>
+                <div className="d-flex">
+                    {promo.products.map(product => {
+                        return(
+                            <Link to={`/product/${product._id}`} className="fast__box" key={product._id}>
+                                <div className="fast__img" style={{
+                                    backgroundImage:`url('http://localhost:3003/${product.img}')`
+                                }}></div>
+                                <div className="fast__title">{product.title}</div>
+                                <div className="fast__genre">{product.category.title}</div>
+                                <div className="fast__old">
+                                    {product.sale>0?(product.price+` so'm`):(product.price*(100-product.sale)/100+' so`m')}  
+                                </div>
+                                <div className="fast__new">
+                                    {product.sale>0?(product.price*(100-product.sale)/100):product.price} so'm
+                                </div>
+                            </Link>
+                        )
+                    })}
                 </div>
             </div>
         </div>
